45481bca739fc4ce5ebb5846b6a96e9842f83f62
Dalete Ferreira <daleteferreirac@gmail.com>
2025-12-09 17:01:55 -0300

    Implement load_pdb function and test file loading in analyzer

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,1 +1,5 @@
 # Main module of the structural PDB analyzer project.
+from utils import load_pdb
+
+atoms = load_pdb("../data/1CRN.pdb")
+print(len(atoms))
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,15 @@
-# Helper functions for the structural analyzer.
+# Helper functions for the structural analyzer
+
+def load_pdb(filepath):
+    """
+    Loads a PDB file.
+    filepath: path to PDB file
+    """
+    atoms_lines = []
+    with open(filepath, "r") as f:
+        for line in f:
+            if line.startswith("ATOM") or line.startswith("HETATM"): # atoms or ligands
+                atoms_lines.append(line.strip()) # removes \n
+
+    return atoms_lines
+
